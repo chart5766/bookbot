a5f9d4d5c34f6635a2fbce73cc822413d425ea4b
chart5766 <8448700+chart5766@users.noreply.github.com>
2024-11-03 20:47:45 -0600

    Added a for loop that prints the letters one by one

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 # print (/Users/christopherhart/workspace/github.com/chart5766/bookbot/books/frankenstein.txt)
 
-f =  open("bookbot/books/frankenstein.txt")
+# f =  open("/Users/christopherhart/workspace/github.com/chart5766/bookbot/books/frankenstein.txt")
+# dict_letters = {}
+# character_list = []
+
+# def count_words(f):
+#    text_read = f.read()
+#    text_split = (text_read.split())
+#    wc =  (len(text_split))
+#    print(wc)
+
+
+# def countLetters():
+   
+#    with open("/Users/christopherhart/workspace/github.com/chart5766/bookbot/books/frankenstein.txt") as f:
+#       text_read = f.read()
+#       text_split = text_read.split()
+
+#       for words in text_split:
+#          for word in words.split():
+
+#           print(words)
+         
+f =  open("/Users/christopherhart/workspace/github.com/chart5766/bookbot/books/frankenstein.txt")
 dict_letters = {}
 
 def count_words(f):
@@ -18,10 +40,30 @@ def countLetters(f):
       lowered_string = words.lower()
       
       for letter in lowered_string:
+         print(letter)
          
    
 
 countLetters(f)
+      
+
+         
+
+      
+
+
+   
+   
+   
+   
+ 
+
+   
+         
+
+      
+
+
       
    
       
